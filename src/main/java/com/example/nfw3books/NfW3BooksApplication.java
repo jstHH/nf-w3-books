b82abd78ca0ad8223fadf980301d30598dec9094
Joachim <joachim.stolle@hotmail.de>
2022-04-05 15:03:26 +0200

    initial commit

---
+++ src/main/java/com/example/nfw3books/NfW3BooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.nfw3books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfW3BooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfW3BooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nfw3books/NfW3BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nfw3books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfW3BooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
